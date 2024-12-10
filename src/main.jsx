511f7bf4914732471a2bf6f24774a9f859b11cd9
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2024-12-10 23:05:59 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -6,5 +7,6 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
